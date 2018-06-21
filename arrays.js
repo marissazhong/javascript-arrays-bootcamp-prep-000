060dc2c329aa95e63155b31854d230a313069e02
Marissa Zhong <mwz2101@columbia.edu>
2018-06-21 21:50:00 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,9 +1,17 @@
 var chocolateBars = ['snickers','hundred grand','kitkat','skittles'];
 
 function addElementToBeginningOfArray(arr, element) {
-  
+  return [element,...arr]
 }
 
 function destructivelyAddElementToBeginningOfArray(arr, element) {
-  
+  return arr.unshift(element)
+}
+
+function addElementToEndOfArray(arr, element) {
+  return [...arr,element]
+}
+
+function destructivelyAddElementToEndOfArray(arr, element) {
+  return arr.shift(element)
 }
