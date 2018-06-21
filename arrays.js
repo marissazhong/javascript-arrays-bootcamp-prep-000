51ca7b9beb2aec2dd5f77d0cddd6176f5b941318
Marissa Zhong <mwz2101@columbia.edu>
2018-06-21 21:47:05 +0000

    automatically backed up by learn

---
+++ arrays.js
@@ -0,0 +1,9 @@
+var chocolateBars = ['snickers','hundred grand','kitkat','skittles'];
+
+function addElementToBeginningOfArray(arr, element) {
+  
+}
+
+function destructivelyAddElementToBeginningOfArray(arr, element) {
+  
+}
